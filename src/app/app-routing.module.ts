9b123f232fb55c8fced2f73f102333ea1a3e34ee
Hugo <hugo.andreas@totvs.com.br>
2020-12-30 15:38:44 -0200

    🚀 commit inicial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'turmas'
+  },
+  {
+    path: 'turmas',
+    loadChildren: () => import('./turmas/turmas.module').then(m => m.TurmasModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
