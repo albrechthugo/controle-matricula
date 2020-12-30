e2be67a63f26f8b59173f49f20e4d4db73578ba4
Hugo <hugo.andreas@totvs.com.br>
2020-12-30 15:41:27 -0200

    adicionado modulo core da aplicacao

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { MenuComponent } from './menu/menu.component';
+
+@NgModule({
+  declarations: [
+    MenuComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
+  exports: [
+    MenuComponent
+  ]
+})
+export class CoreModule { }
