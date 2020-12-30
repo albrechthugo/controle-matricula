c7c2b9989a501017a9f11fdb80061189efff1c88
Hugo <hugo.andreas@totvs.com.br>
2020-12-30 15:43:20 -0200

    adicionado modulo de turmas e componente principal do modulo

---
+++ src/app/turmas/turmas-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LandingComponent } from './landing/landing.component';
+
+const turmasRoutes: Routes = [
+  {
+    path: '',
+    component: LandingComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(turmasRoutes)],
+  exports: [RouterModule]
+})
+export class TurmasRoutingModule { }
---
+++ src/app/turmas/turmas.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { TurmasRoutingModule } from './turmas-routing.module';
+import { TurmasComponent } from './turmas.component';
+import { LandingComponent } from './landing/landing.component';
+
+@NgModule({
+  declarations: [
+    TurmasComponent, 
+    LandingComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    TurmasRoutingModule
+  ]
+})
+export class TurmasModule { }
