483518d132e6ee05b1651ff5394f7db64ccb843c
Hugo <hugo.andreas@totvs.com.br>
2021-01-04 09:29:58 -0200

    pagina de boas vindas

--- src/app/turmas/landing/landing.component.ts
+++ src/app/turmas/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
+    
 }
