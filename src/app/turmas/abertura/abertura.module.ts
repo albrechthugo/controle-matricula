d3d8a19b2d827d6bbabb3213fcf20e8bab87a193
Hugo <hugo.andreas@totvs.com.br>
2021-01-05 13:50:22 -0200

    modulo da abertura de turmas

---
+++ src/app/turmas/abertura/abertura.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { 
+PoButtonModule, 
+PoContainerModule, 
+PoFieldModule, 
+PoModalModule, 
+PoStepperModule } from '@po-ui/ng-components';
+import { AberturaRoutingModule } from './abertura-routing.module';
+
+import { AberturaComponent } from './abertura.component';
+import { InformacoesBasicasComponent } from './informacoes-basicas/informacoes-basicas.component';
+import { DisciplinasComponent } from './disciplinas/disciplinas.component';
+import { AlunosComponent } from './alunos/alunos.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AberturaComponent,
+    InformacoesBasicasComponent,
+    DisciplinasComponent,
+    AlunosComponent
+  ],
+  imports: [
+    CommonModule,
+    PoStepperModule,
+    PoContainerModule,
+    RouterModule,
+    AberturaRoutingModule,
+    PoButtonModule,
+    PoFieldModule,
+    PoModalModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ]
+})
+export class AberturaModule { }
