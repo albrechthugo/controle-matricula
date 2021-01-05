029545412226cc1ba596da3dc8ec7675a6c5c260
Hugo <hugo.andreas@totvs.com.br>
2021-01-05 16:27:13 -0200

    consumindo os dados da api

--- src/app/turmas/abertura/alunos/alunos.component.ts
+++ src/app/turmas/abertura/alunos/alunos.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { PoMultiselectOption } from '@po-ui/ng-components';
 
+import { Aluno } from 'src/app/entities/aluno/aluno.interface';
+import { AlunosGetAllService } from './services/alunos-get-all.service';
+
 @Component({
   selector: 'app-alunos',
   templateUrl: './alunos.component.html',
@@ -8,11 +11,18 @@ import { PoMultiselectOption } from '@po-ui/ng-components';
 })
 export class AlunosComponent implements OnInit {
 
+  alunos: Aluno[] = [];
   options: PoMultiselectOption[] = [];
 
-  constructor() { }
+  constructor(private alunosGetAllService: AlunosGetAllService) { }
 
   ngOnInit(): void {
+    this.alunosGetAllService.getAllAlunos().subscribe(alunos => {
+      this.alunos = alunos;
+      this.alunos.map(aluno => {
+        this.options = [...this.options, { label: aluno.nome, value: aluno.id }];
+      })
+    })
   }
 
 }
--- src/app/turmas/abertura/disciplinas/disciplinas.component.ts
+++ src/app/turmas/abertura/disciplinas/disciplinas.component.ts
@@ -1,5 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { PoMultiselectOption, PoSelectOption } from '@po-ui/ng-components';
+import { Disciplina } from 'src/app/entities/disciplina/disciplina.interface';
+
+import { DisciplinaGetAllService } from './services/disciplina-get-all.service';
 
 @Component({
   selector: 'app-disciplinas',
@@ -10,17 +13,23 @@ export class DisciplinasComponent implements OnInit {
   
   options: PoMultiselectOption[] = [];
   professoresOptions: PoSelectOption[] = [];
+  disciplinas: Disciplina[] = [];
 
   @Output()
   nextStep: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  constructor(private disciplinasGetAllService: DisciplinaGetAllService) { }
 
   ngOnInit(): void {
+    this.disciplinasGetAllService.getAllDisciplinas().subscribe(disciplinas => {
+      this.disciplinas = disciplinas;
+      this.disciplinas.map(disciplina => {
+        this.options = [...this.options, { label: disciplina.descricao, value: disciplina.id }]
+      })
+    })
   }
 
   next() {
     this.nextStep.emit();
   }
-
 }
