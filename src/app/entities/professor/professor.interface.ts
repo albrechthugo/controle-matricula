50f5901b53346d154aeb44499d0d9ed06a77cd8e
Hugo <hugo.andreas@totvs.com.br>
2021-01-05 13:44:51 -0200

    tipando o array de disciplinas dos professores e turmas

--- src/app/entities/professor/professor.interface.ts
+++ src/app/entities/professor/professor.interface.ts
@@ -1,7 +1,9 @@
 import { Pessoa } from '../pessoa/pessoa.interface';
 import { Titulacao } from './titulacao/titulacao.enum';
+import { Disciplina } from '../disciplina/disciplina.interface';
 
 export interface Professor extends Pessoa {
   id?: number,
-  titulacao?: Titulacao
+  titulacao?: Titulacao,
+  disciplinasMinistradas: Disciplina[];
 }
--- src/app/entities/turma/turma.interface.ts
+++ src/app/entities/turma/turma.interface.ts
@@ -1,7 +1,10 @@
+import { Disciplina } from "../disciplina/disciplina.interface";
+
 export interface Turma {
   id?: number,
   descricao?: string,
   anoLetivo: number,
   periodoLetivo: number,
-  numeroVagas: number
+  numeroVagas: number,
+  disciplinas: Disciplina[];
 }
