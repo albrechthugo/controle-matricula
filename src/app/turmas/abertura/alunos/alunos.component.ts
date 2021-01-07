eec4edae8f983c6af31cb582b8ccdf90056c8066
Hugo <hugo.andreas@totvs.com.br>
2021-01-07 10:09:44 -0200

    utilizando os servicos de criar e buscar dados

--- src/app/turmas/abertura/alunos/alunos.component.ts
+++ src/app/turmas/abertura/alunos/alunos.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { PoMultiselectOption } from '@po-ui/ng-components';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+import {PoMultiselectOption} from '@po-ui/ng-components';
 
-import { Aluno } from 'src/app/entities/aluno/aluno.interface';
+import {Aluno} from 'src/app/entities/aluno/aluno.interface';
+import { AlunosCriarService } from './services/alunos-criar.service';
 import { AlunosGetAllService } from './services/alunos-get-all.service';
 
 @Component({
@@ -11,18 +13,44 @@ import { AlunosGetAllService } from './services/alunos-get-all.service';
 })
 export class AlunosComponent implements OnInit {
 
-  alunos: Aluno[] = [];
   options: PoMultiselectOption[] = [];
 
-  constructor(private alunosGetAllService: AlunosGetAllService) { }
+  form = this.fb.group({
+    cpf: null,
+    email: null,
+    nome: null,
+    formaIngresso: null,
+    matricula: null
+  })
+
+  alunos: Aluno[] = [];
+
+  aluno: Aluno = {
+    cpf: null,
+    email: null,
+    nome: null,
+    formaIngresso: null,
+    matricula: null
+  }
+
+  constructor(
+    private alunosGetAllService: AlunosGetAllService,
+    private alunosCriarService: AlunosCriarService,
+    private fb: FormBuilder
+    ) {}
+
 
   ngOnInit(): void {
     this.alunosGetAllService.getAllAlunos().subscribe(alunos => {
       this.alunos = alunos;
       this.alunos.map(aluno => {
         this.options = [...this.options, { label: aluno.nome, value: aluno.id }];
-      })
-    })
+      });
+    });
   }
 
+  criarAluno(): void {
+    this.alunosCriarService.criarAluno(this.aluno)
+      .subscribe(() => console.log('Aluno criado com sucesso!'));
+  }
 }
--- src/app/turmas/abertura/disciplinas/disciplinas.component.ts
+++ src/app/turmas/abertura/disciplinas/disciplinas.component.ts
@@ -1,8 +1,13 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { PoMultiselectOption, PoSelectOption } from '@po-ui/ng-components';
-import { Disciplina } from 'src/app/entities/disciplina/disciplina.interface';
 
+import {Disciplina} from 'src/app/entities/disciplina/disciplina.interface';
 import { DisciplinaGetAllService } from './services/disciplina-get-all.service';
+import { DisciplinasCriarService } from './services/disciplinas-criar.service';
+
+import {ProfessoresGetAllService} from '../../../services/professores/professores-get-all.service';
+import {Professor} from '../../../entities/professor/professor.interface';
+import {ProfessoresCriarService} from '../../../services/professores/professores-criar.service';
 
 @Component({
   selector: 'app-disciplinas',
@@ -10,23 +15,66 @@ import { DisciplinaGetAllService } from './services/disciplina-get-all.service';
   styleUrls: ['./disciplinas.component.css']
 })
 export class DisciplinasComponent implements OnInit {
-  
-  options: PoMultiselectOption[] = [];
-  professoresOptions: PoSelectOption[] = [];
+
+  disciplinasOptions: PoMultiselectOption[] = [];
   disciplinas: Disciplina[] = [];
+  disciplina: Disciplina = {
+    sigla: null,
+    cargaHoraria: null,
+    descricao: null,
+    professor: null
+  }
+
+  professoresOptions: PoSelectOption[] = [];
+  professores: Professor[] = [];
+  professor: Professor = {
+    nome: null,
+    email: null,
+    cpf: null,
+    titulacao: null,
+    disciplinasMinistradas: []
+  }
 
   @Output()
   nextStep: EventEmitter<any> = new EventEmitter();
 
-  constructor(private disciplinasGetAllService: DisciplinaGetAllService) { }
+  constructor(
+    private disciplinasGetAllService: DisciplinaGetAllService,
+    private professoresGetAllService: ProfessoresGetAllService,
+    private professoresCriarService: ProfessoresCriarService,
+    private disciplinasCriarService: DisciplinasCriarService
+  ) { }
 
   ngOnInit(): void {
-    this.disciplinasGetAllService.getAllDisciplinas().subscribe(disciplinas => {
-      this.disciplinas = disciplinas;
-      this.disciplinas.map(disciplina => {
-        this.options = [...this.options, { label: disciplina.descricao, value: disciplina.id }]
+    this.disciplinasGetAllService.getAllDisciplinas()
+      .subscribe(disciplinas => {
+        this.disciplinas = disciplinas;
+        this.disciplinas.map(disciplina => {
+          this.disciplinasOptions = [...this.disciplinasOptions, { label: disciplina.descricao, value: disciplina.id }];
       })
     })
+
+    this.professoresGetAllService.getAllProfessores()
+      .subscribe(professores => {
+        this.professores = professores;
+        this.professores.map(professor => {
+          this.professoresOptions = [...this.professoresOptions, { label: professor.nome, value: professor.id }];
+        })
+      })
+  }
+
+  criarDisciplina(): void {
+    this.disciplinasCriarService.criarDisciplina(this.disciplina)
+      .subscribe(() => {
+        console.log('Disciplina criada com sucesso!');
+      })
+  }
+
+  criarProfessor(): void {
+    this.professoresCriarService.criarProfessor(this.professor)
+      .subscribe(() => {
+        console.log('Professor criado com sucesso!')
+      });
   }
 
   next() {
