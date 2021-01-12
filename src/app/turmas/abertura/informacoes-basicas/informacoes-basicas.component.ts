3e0b561044531f81fa43c296e2d448bc73b6ad60
Hugo <hugo.andreas@totvs.com.br>
2021-01-12 10:58:56 -0200

    adicionar dados basicos para abertura da turma

--- src/app/turmas/abertura/informacoes-basicas/informacoes-basicas.component.ts
+++ src/app/turmas/abertura/informacoes-basicas/informacoes-basicas.component.ts
@@ -1,4 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
+import { Turma } from '../../../entities/turma/turma.interface';
+import { novaTurma } from '../../../shared/mocks/turma-mock';
 
 @Component({
   selector: 'app-informacoes-basicas',
@@ -10,12 +14,43 @@ export class InformacoesBasicasComponent implements OnInit {
   @Output()
   nextStep: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  informacoesBasicas: Turma = {
+    descricao: null,
+    anoLetivo: null,
+    periodoLetivo: null,
+    numeroVagas: null
+  }
+
+  form = new FormGroup({
+    descricao: new FormControl(),
+    anoLetivo: new FormControl(),
+    periodoLetivo: new FormControl(),
+    numeroVagas: new FormControl()
+  })
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
   }
 
+  saveInfo(): void {
+    novaTurma.descricao = this.informacoesBasicas.descricao;
+    novaTurma.anoLetivo = this.informacoesBasicas.anoLetivo;
+    novaTurma.periodoLetivo = this.informacoesBasicas.periodoLetivo;
+    novaTurma.numeroVagas = this.informacoesBasicas.numeroVagas;
+  }
+
+  // validateForm(): void {
+  //   this.form = this.fb.group({
+  //     descricao: [null, [Validators.maxLength(40), Validators.required]],
+  //     anoLetivo: [null, [Validators.maxLength(4), Validators.required]],
+  //     periodoLetivo: [null, [Validators.maxLength(1), Validators.required]],
+  //     numeroVagas: [null, [Validators.maxLength(2), Validators.required]]
+  //   })
+  // }
+
   next() {
+    this.saveInfo();
     this.nextStep.emit();
   }
 
