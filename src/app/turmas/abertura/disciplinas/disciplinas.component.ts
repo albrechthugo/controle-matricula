fd56bf02d7bb3624cd04cf790be4912083273d36
Hugo <hugo.andreas@totvs.com.br>
2021-01-12 11:00:20 -0200

    permitir criar disciplinas/professores e adiciona-las na turma

--- src/app/turmas/abertura/disciplinas/disciplinas.component.ts
+++ src/app/turmas/abertura/disciplinas/disciplinas.component.ts
@@ -1,13 +1,16 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { PoMultiselectOption, PoSelectOption } from '@po-ui/ng-components';
 
-import {Disciplina} from 'src/app/entities/disciplina/disciplina.interface';
-import { DisciplinaGetAllService } from './services/disciplina-get-all.service';
-import { DisciplinasCriarService } from './services/disciplinas-criar.service';
+import { Disciplina } from 'src/app/entities/disciplina/disciplina.interface';
 
-import {ProfessoresGetAllService} from '../../../services/professores/professores-get-all.service';
-import {Professor} from '../../../entities/professor/professor.interface';
+import { Professor } from '../../../entities/professor/professor.interface';
+import { Titulacao } from '../../../entities/professor/titulacao/titulacao.enum';
+import {DisciplinaGetAllService} from './services/disciplina-get-all.service';
 import {ProfessoresCriarService} from '../../../services/professores/professores-criar.service';
+import {DisciplinasCriarService} from './services/disciplinas-criar.service';
+import {ProfessoresGetAllService} from '../../../services/professores/professores-get-all.service';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { novaTurma } from 'src/app/shared/mocks/turma-mock';
 
 @Component({
   selector: 'app-disciplinas',
@@ -16,8 +19,13 @@ import {ProfessoresCriarService} from '../../../services/professores/professores
 })
 export class DisciplinasComponent implements OnInit {
 
+  @Output()
+  nextStep: EventEmitter<any> = new EventEmitter();
+
   disciplinasOptions: PoMultiselectOption[] = [];
+
   disciplinas: Disciplina[] = [];
+
   disciplina: Disciplina = {
     sigla: null,
     cargaHoraria: null,
@@ -25,59 +33,98 @@ export class DisciplinasComponent implements OnInit {
     professor: null
   }
 
+  disciplinaForm = new FormGroup({
+    sigla: new FormControl(),
+    cargaHoraria: new FormControl(),
+    descricao: new FormControl(),
+    professor: new FormControl()
+  });
+
   professoresOptions: PoSelectOption[] = [];
+
   professores: Professor[] = [];
+
   professor: Professor = {
     nome: null,
     email: null,
     cpf: null,
     titulacao: null,
-    disciplinasMinistradas: []
+    disciplinasMinistradas: null
   }
 
-  @Output()
-  nextStep: EventEmitter<any> = new EventEmitter();
+  titulacaoProfessorOptions: PoSelectOption[] = [
+    { label: 'Mestre', value: Titulacao.MESTRE },
+    { label: 'Doutor', value: Titulacao.DOUTOR },
+    { label: 'PHD', value: Titulacao.PHD }
+  ];
+
+  professorForm = new FormGroup({
+    nome: new FormControl(),
+    email: new FormControl(),
+    cpf: new FormControl(),
+    titulacao: new FormControl()
+  });
 
   constructor(
-    private disciplinasGetAllService: DisciplinaGetAllService,
-    private professoresGetAllService: ProfessoresGetAllService,
-    private professoresCriarService: ProfessoresCriarService,
-    private disciplinasCriarService: DisciplinasCriarService
-  ) { }
+    private criarDisciplinaService: DisciplinasCriarService,
+    private getAllDisciplinaService: DisciplinaGetAllService,
+    private criarProfessorService: ProfessoresCriarService,
+    private getAllProfessorService: ProfessoresGetAllService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
-    this.disciplinasGetAllService.getAllDisciplinas()
+    this.getAllDisciplinaService.getAllDisciplinas()
       .subscribe(disciplinas => {
         this.disciplinas = disciplinas;
         this.disciplinas.map(disciplina => {
           this.disciplinasOptions = [...this.disciplinasOptions, { label: disciplina.descricao, value: disciplina.id }];
-      })
-    })
+        })
+      });
 
-    this.professoresGetAllService.getAllProfessores()
+    this.getAllProfessorService.getAllProfessores()
       .subscribe(professores => {
         this.professores = professores;
         this.professores.map(professor => {
           this.professoresOptions = [...this.professoresOptions, { label: professor.nome, value: professor.id }];
         })
-      })
-  }
-
-  criarDisciplina(): void {
-    this.disciplinasCriarService.criarDisciplina(this.disciplina)
-      .subscribe(() => {
-        console.log('Disciplina criada com sucesso!');
-      })
-  }
-
-  criarProfessor(): void {
-    this.professoresCriarService.criarProfessor(this.professor)
-      .subscribe(() => {
-        console.log('Professor criado com sucesso!')
       });
   }
 
-  next() {
+  criarDisciplina(): void {
+    this.criarDisciplinaService.criarDisciplina(this.disciplina)
+      .subscribe(() => console.log('Disciplina criada com sucesso!'));
+  }
+
+  criarProfessor(): void {
+    this.criarProfessorService.criarProfessor(this.professor)
+      .subscribe(() => console.log('Professor criado com sucesso!'));
+  }
+
+  saveInfo(): void {
+    novaTurma.disciplinas = this.disciplinas;
+  }
+
+  // validateDisciplinaForm(): void {
+  //   this.disciplinaForm = this.fb.group({
+  //     sigla: [null, [Validators.maxLength(4), Validators.required]],
+  //     cargaHoraria: [null, [Validators.maxLength(2), Validators.required]],
+  //     descricao: [null, [Validators.maxLength(4), Validators.required]],
+  //     professor: [null, Validators.required]
+  //   })
+  // }
+
+  // validateProfessorForm(): void {
+  //   this.professorForm = this.fb.group({
+  //     nome: [null, [Validators.maxLength(40), Validators.required]],
+  //     email: [null, [Validators.email, Validators.required]],
+  //     cpf: [null, [Validators.required, Validators.pattern(/[0-9]{3}[.][0-9]{3}[.][0-9]{3}[-][0-9]{2}/)]],
+  //     titulacao: [null, Validators.required]
+  //   })
+  // }
+
+  next(): void {
+    this.saveInfo();
     this.nextStep.emit();
   }
 }
