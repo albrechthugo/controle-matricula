7fbc4dcb9e4799058b817a33982e23889cfc3dfd
Hugo <hugo.andreas@totvs.com.br>
2021-01-12 18:07:10 -0200

    implementando resolver nas rotas

--- src/app/turmas/abertura/abertura-routing.module.ts
+++ src/app/turmas/abertura/abertura-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfessoresGetAllResolver } from 'src/app/resolvers/professores/professores-get-all.resolver';
 
 import { AberturaComponent } from './abertura.component';
+import { AlunosGetAllResolver } from './alunos/resolvers/alunos-get-all.resolver';
+import { DisciplinasGetAllResolver } from './disciplinas/resolvers/disciplinas-get-all.resolver';
 
 const aberturaRoutes: Routes = [
   {
     path: '',
     component: AberturaComponent,
+    resolve: {
+      professores: ProfessoresGetAllResolver,
+      alunos: AlunosGetAllResolver,
+      disciplinas: DisciplinasGetAllResolver
+    }
   }
 ];
 
