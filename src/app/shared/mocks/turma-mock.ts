2d8c99d36ae6b653a1c8af8a5f62cae4a5b4d9e1
Hugo <hugo.andreas@totvs.com.br>
2021-01-12 10:48:08 -0200

    criado um mock para uma turma sendo criada

---
+++ src/app/shared/mocks/turma-mock.ts
@@ -0,0 +1,10 @@
+import { Turma } from '../../entities/turma/turma.interface';
+
+export const novaTurma: Turma = {
+  descricao: null,
+  anoLetivo: null,
+  periodoLetivo: null,
+  numeroVagas: null,
+  alunos: null,
+  disciplinas: null
+}
