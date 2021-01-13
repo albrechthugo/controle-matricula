a77ae31e2cdaeea2f90839f6f981a405d3488812
Hugo <hugo.andreas@totvs.com.br>
2021-01-13 14:11:01 -0200

    🚧 utilizando menu do po-ui

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { PoMenuModule } from '@po-ui/ng-components';
 
 import { MenuComponent } from './menu/menu.component';
 
@@ -11,6 +12,7 @@ import { MenuComponent } from './menu/menu.component';
   imports: [
     CommonModule,
     RouterModule,
+    PoMenuModule
   ],
   exports: [
     MenuComponent
--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PoMenuItem } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-menu',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+
+  navItems: PoMenuItem[] = [
+    {
+      label: 'Início',
+      link: 'turmas',
+      icon: 'po-icon po-icon-home'
+    },
+    {
+      label: 'Abrir Turma',
+      link: 'turmas/abertura',
+      icon: 'po-icon po-icon-warehouse'
+    }
+  ];
 
   constructor() { }
 
