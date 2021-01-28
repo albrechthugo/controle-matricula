62046de8b2a6e72b6bde63baa75c52420c7c9783
Hugo <albrechthugo4@gmail.com>
2021-01-28 17:31:14 -0300

    fix: removendo emmiter para reset de form

--- src/app/services/turma/turma-criar.service.ts
+++ src/app/services/turma/turma-criar.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Turma } from 'src/app/entities/turma/turma.interface';
 
@@ -8,12 +8,9 @@ import { Turma } from 'src/app/entities/turma/turma.interface';
 })
 export class TurmaCriarService {
 
-  canResetForm = new EventEmitter<Turma>();
-
   constructor(private http: HttpClient) { }
 
   abrirTurma(turma: Turma): Observable<Turma> {
-    this.canResetForm.emit(turma)
     return this.http.post<Turma>('api/turmas', turma);
   }
 }
